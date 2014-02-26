a5e51f2af5a32f98c4b20392cd35bcd5f4f6591e
Oleg Korsunsky <jatruadmin@gmail.com>
2014-02-27 01:52:36 +0400

    js cleanup, share section

--- js/init.js
+++ js/init.js
@@ -1,3 +1,33 @@
+function loadScript(path, callback, errorCallback) {
+    var done = false,
+        script = document.createElement('script');
+
+    script.onload = function() {
+        if (!done) {
+            done = true;
+            callback && callback();
+        }
+    };
+
+    script.onreadystatechange = function() {
+        if (!done && (script.readyState === 'complete' || script.readyState === 'loaded')) {
+            done = true;
+            callback && callback();
+        }
+    };
+
+    script.onerror = function() {
+        if (!done) {
+            done = true;
+            errorCallback && errorCallback();
+        }
+    };
+
+    script.async = true;
+    script.src = path;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
+}
+
 pepFormController = function() {
     var pepForm = document.getElementsByClassName('js-pepyaka-generator');
 
@@ -10,6 +40,7 @@ pepFormController = function() {
         showCodeButtons = $('.js-show-code'),
         codeOutputArea = $('.js-code-output'),
         val = pepInput[0].value,
+        lastVal = '',
         fonts = [],
         picArr = [],
         activeMarkup = 'html',
@@ -36,7 +67,7 @@ pepFormController = function() {
 
         if (localStorageAvailable) localStorage.setItem('fonts', JSON.stringify(fonts));
 
-        getGifs();
+        getLetters(true);
         showCode();
     }).change();
 
@@ -51,7 +82,7 @@ pepFormController = function() {
         }
 
         if (val != pepInput[0].value || e.keyCode == 13) {
-            getGifs(e.keyCode == 13);
+            getLetters(e.keyCode == 13);
             showCode();
         }
     });
@@ -65,11 +96,11 @@ pepFormController = function() {
     });
 
     function showCode(markup) {
-        codeOutputArea.html(Pepyaka.generateMarkup(picArr, {
+        codeOutputArea[0].value = Pepyaka.generateMarkup(picArr, {
             markupName: activeMarkup,
             includeLink: true,
             includeDomain: true
-        }));
+        });
 
         codeOutputArea.off('focus.select').one('focus.select', function() {
             var _this = this;
@@ -79,8 +110,8 @@ pepFormController = function() {
             }, 0);
         });
     }
-    var lastVal = '';
-    function getGifs( regenerate ) {
+
+    function getLetters( regenerate ) {
         var i, _i, pos1, pos2, part1, part2, length1, length2, maxPos1, maxPos2, lastLength, max1, max2;
         val = pepInput[0].value;
         if( regenerate ){
@@ -91,7 +122,7 @@ pepFormController = function() {
                     for( i = 0, _i = lastVal.length; i < _i; i++ )
                         if( lastVal.charAt(i) !== val.charAt(i) )
                             picArr[i] = Pepyaka.getGifs(val.charAt(i), fonts)[0];
-                }else if( true ){//val.length > lastVal.length ){
+                }else{
                     part1 = '';
                     part2 = lastVal;
                     length1 = 0;
@@ -120,7 +151,6 @@ pepFormController = function() {
                             pos2 === -1 && (pos2 = false);
                         }else
                             pos2 = false;
-                        //console.log(part1,part2,pos1,pos2)
                         if( pos1 === false || pos2 === false || pos1+length1 < pos2 ){
                             if( pos1 !== false && length1 > max1 ){
                                 max1 = length1;
@@ -159,10 +189,7 @@ pepFormController = function() {
                     }else{
                         picArr = Pepyaka.getGifs(val, fonts);
                     }
-                }else{
-                    picArr = Pepyaka.getGifs(val, fonts);
                 }
-
             }
         }
         lastVal = val;
@@ -172,7 +199,6 @@ pepFormController = function() {
 }
 
 $(function() {
-
     $('.js-toggle-font-settings').on('click', function(e) {
         $('.js-font-settings').toggleClass('hidden');
     });
@@ -189,98 +215,30 @@ $(function() {
 
     pepFormController();
     pepInput.focus();
-});
 
-/*
-Pepyaka = {getGifs:function(a){ return a.split('').map(function(a){return [a];});}};
-fonts = 8
+    /* SOCIAL */
+    if (window.matchMedia && window.matchMedia('all and (min-width: 60em)').matches) {
+        $(window).load(function() { /* loading this crap after everything else*/
+            $('.js-share-buttons').removeClass('is-inactive').addClass('is-active');
 
-var test = function(lastVal, val){
-    picArr = Pepyaka.getGifs(lastVal)
-    if( lastVal !== val ){
-        if( val.length === lastVal.length ){
-            for( i = 0, _i = lastVal.length; i < _i; i++ )
-                if( lastVal.charAt(i) !== val.charAt(i) )
-                    picArr[i] = Pepyaka.getGifs(val.charAt(i), fonts)[0];
-        }else if( val.length > lastVal.length ){
-            part1 = '';
-            part2 = lastVal;
-            length1 = 0;
-            lastLength = length2 = lastVal.length;
+            /* g+ */
+            window.___gcfg = {lang: 'ru'};
+            loadScript('https://apis.google.com/js/platform.js');
 
-            max1 = 0;
-            max2 = 0;
-            maxPos1 = 0;
-            maxPos2 = 0;
-            for( i = 0, _i = lastLength + 1; i < _i;
-                 i++,
-                     part1 += part2.charAt(0),
-                     part2 = part2.substr(1),
-                     length1++,
-                     length2--
-                ){
+            /* twitter */
+            loadScript('http://platform.twitter.com/widgets.js');
 
-                if( i > 0 ){
-                    pos1 = val.indexOf(part1);
-                    pos1 === -1 && (pos1 = false);
-                }else
-                    pos1 = false;
+            /* vk */
+            loadScript('http://vk.com/js/api/openapi.js?105', function() {
+                VK.init({apiId: 4213563, onlyWidgets: true});
+                VK.Widgets.Like("vk_like", {type: "button"});
+            });
 
-                if( i < _i - 1 ){
-                    pos2 = val.indexOf(part2,pos1!==false?pos1+length1:0);
-                    pos2 === -1 && (pos2 = false);
-                }else
-                    pos2 = false;
-                console.log(part1,part2,pos1,pos2)
-                if( pos1 === false || pos2 === false || pos1+length1 < pos2 ){
-                    if( pos1 !== false && length1 > max1 ){
-                        max1 = length1;
-                        maxPos1 = pos1;
-                    }
-                    if( maxPos2 < maxPos1 + max1 ){
-                        max2 = false;
-                    }
-                    if( pos2 !== false && length2 > max2 ){
-                        max2 = length2;
-                        maxPos2 = pos2;
-                    }
-                }
-                if( max1 + max2 >= lastLength )
-                    break;
+            /* facebook */
+            loadScript('http://connect.facebook.net/ru_RU/all.js#xfbml=1');
 
-            }
-            if(max1 + max2 > lastLength){ // case where we have an intersection and length of part1+part2 > length of last value
-                maxPos2 += max2;
-                max2 = _i - max1 - 1;
-                maxPos2 -= max2;
-            }
-            if( max1 > 0 || max2 > 0 ){
-                console.log(picArr)
-                picArr = Array.prototype.concat.apply([],[
-                    max1 > 0 ? Pepyaka.getGifs( val.substr(0, maxPos1), fonts ) : [],
-                    max1 > 0 ? picArr.slice( maxPos1, max1 ) : [],
-                    max1 > 0 && max2 > 0 ? (Pepyaka.getGifs( val.substr(maxPos1+max1, maxPos2-maxPos1-max1), fonts )) :
-                        ( max1 > 0 ? Pepyaka.getGifs( val.substr(maxPos1+max1), fonts ) : []),
-                    max2 > 0 ? picArr.slice( picArr.length - max2 ) : [],
-                    max2 > 0 ? Pepyaka.getGifs( val.substr( maxPos1 + max1, maxPos2-maxPos1-max1-3), fonts ) : [],
-                ])
-                console.log(picArr)
-            }else{
-                picArr = Pepyaka.getGifs(val, fonts);
-            }
-            console.log(max1,max2,maxPos1,maxPos2)
-        }else{
-            picArr = Pepyaka.getGifs(val, fonts);
-        }
-
+            /* mail.ru */
+            loadScript('http://cdn.connect.mail.ru/js/loader.js');
+        });
     }
-    if( picArr.join('') !== val )
-        throw (picArr.join('') +'!='+ val)
-    return picArr;
-}
-test('12345','127745');
-
-test('123123','12377321');
-
-test('12345','12772345');
-test('12345','789719999');*/
+});
