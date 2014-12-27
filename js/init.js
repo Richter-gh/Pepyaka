8ab7d9d07967ed5ba694b6d9fe92f30daf8ff65a
Oleg Korsunsky <jatruadmin@gmail.com>
2014-12-27 03:03:04 +0300

    update focus-fix

--- js/init.js
+++ js/init.js
@@ -340,18 +340,27 @@ $(function() {
 
     $('.js-tabs').dTabs();
 
-    /* prevent focus after mouse clicks */  
-    body.addEventListener('mouseup', function(event) {
-        setTimeout(function() {
-            var activeElement = document.activeElement;
+    /* focus-fix | https://github.com/wilddeer/focus-fix/ | CC0 */
+    (function() {
+        var mouseFocusedClass = 'is-mouse-clicked';
 
-            if (activeElement && activeElement !== body) {
-                $(activeElement).addClass('is-mouse-clicked').one('blur', function(){
-                    $(activeElement).removeClass('is-mouse-clicked');
-                });
-            }
-        }, 0);
-    });
+        $(document.body).on('mouseup', function() {
+            //wait for `document.activeElement` to change
+            setTimeout(function() {
+                //find focused element
+                var activeElement = document.activeElement,
+                    $activeElement = $(activeElement);
+
+                //if found and it's not body...
+                if (activeElement && activeElement !== document.body) {
+                    //add special class, remove it after `blur`
+                    $activeElement.addClass(mouseFocusedClass).one('blur', function() {
+                        $activeElement.removeClass(mouseFocusedClass);
+                    });
+                }
+            }, 0);
+        });
+    })();
 
     /* SOCIAL AND METRIKA */
     $(window).load(function() { /* loading this crap after everything else*/
